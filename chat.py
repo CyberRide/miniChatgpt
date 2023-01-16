47f57bb424465aa0de052c71f822207291809842
cyberide <cyberide@localhost.localdomain>
2023-01-16 20:55:37 +0100

    chatgptmini

---
+++ chat.py
@@ -0,0 +1,20 @@
+import openai
+import gradio as gr
+x
+openai.api_key='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'
+def chatbot(text):
+    return openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=text,
+        max_tokens=1024,
+        n=1,
+        temperature=0.5,
+    ).choices[0].text.strip()
+def gradio_interface(prompt, history=[]):
+    output = chatbot(prompt)
+    history.append((prompt, output))
+    return history, history
+
+gr.Interface(fn = gradio_interface,
+             inputs = ["text",'state'],
+             outputs = ["chatbot",'state']).launch(debug = False, share=True)
